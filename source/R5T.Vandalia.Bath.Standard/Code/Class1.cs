e3549f057a11c39adbd756710b899d9733e00268
MinexAutomation <minex.automation@gmail.com>
2020-01-25 02:21:30 -0800

    Added IServiceCollection extensions.

--- source/R5T.Vandalia.Bath.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Vandalia.Bath.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Vandalia.Bath.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath.Standard;
+
+
+namespace R5T.Vandalia.Bath.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// See adds an <see cref="IValueEqualityComparer{T}"/> service for the <typeparamref name="TValue"/> type.
+        /// </summary>
+        public static IServiceCollection AddValueEqualityComparer<TValue, TValueEqualityComparer>(this IServiceCollection services)
+            where TValueEqualityComparer: HumanOutputValueEqualityComparer<TValue>
+        {
+            services
+                .AddSingleton<IValueEqualityComparer<TValue>, TValueEqualityComparer>()
+                .AddHumanOutput()
+                ;
+
+            return services;
+        }
+    }
+}
